3770cab911bd6d9a71ac7760a31ebd90c007c078
jesus-2808 <jesus.gonzalez.munoz.al@iespoligonosur.org>
2022-02-09 19:04:26 +0100

    porsi

--- views/signupView.php
+++ views/signupView.php
@@ -7,7 +7,7 @@
   <meta name="description" content="">
   <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
   <meta name="generator" content="Hugo 0.88.1">
-  <title>Inicio Sesión</title>
+  <title>Registrate para ver tus puntuaciones</title>
 
   <!-- Latest compiled and minified CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -17,7 +17,7 @@
 
   <style>
     .container {
-      background-image: url(images/alhambraLogin.jpg);
+      background-image: url(./images/alhambraLogin.jpg);
       background-size: 100% auto;
       background-position: center top;
       background-repeat: no-repeat;
@@ -80,27 +80,27 @@
 
 
   <!-- Custom styles for this template -->
-
+  <link href="signin.css" rel="stylesheet">
 </head>
 
 <body class="text-center container">
   
   <main class="form-signin">
-    <form method="POST" action="index.php?controller=usuarios&action=signup">
+    <form method="POST" action="index.php?controller=usuarios&action=login">
       <h1 class="text-decoration-underline">TITULO</h1><br>
-      <h1 class="h3 mb-3 fw-normal">Inicia Sesion</h1>
+      <h1 class="h3 mb-3 fw-normal">Regístrate en el juego!</h1>
 
       <div class="form-floating">
-        <input type="text" name="usuario" class="form-control opacity-75" id="floatingInput" placeholder="¿con qué nombre quieres jugar?" required>
+        <input type="text" name="usuario" class="form-control opacity-75" id="floatingInput" placeholder="Usuario" required>
         <label for="floatingInput">Usuario</label>
       </div>
       <div class="form-floating">
-        <input type="password" class="form-control opacity-75" id="floatingPassword" placeholder="Introduce tu nueva contraseña">
+        <input type="password" class="form-control opacity-75" id="floatingPassword" placeholder="Contraseña">
         <label for="floatingPassword">Contraseña</label>
       </div>
-      <p><input class="w-100 btn btn-lg btn-primary" type="submit" value="Iniciar Sesion" /></p>
+      <p><input class="w-100 btn btn-lg btn-primary" type="submit" value="Registrar usuario" /></p>
       
-      <p class="text-white">¿No tienes una cuenta? <a href="views/signupView.php" class="text-white"  type="submit">Registrate ahora</a></p>
+      <p class="text-white">¿Ya estas registrado? <a href="loginView.php" class="text-white"  type="submit">Accede al Login</a></p>
       <p class="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
     </form>
   </main>
