a7be55f4bce8c1d2cffd37047735bb49398a42bc
jesus-2808 <jesus.gonzalez.munoz.al@iespoligonosur.org>
2022-02-10 20:14:59 +0100

    menu

--- views/menu.php
+++ views/menu.php
@@ -10,7 +10,36 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Latest compiled JavaScript -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"> </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script>
+
+$(document).ready(function(){
+  $("#escudo").click(function(){
+    $(this).animate({height: "300px"});
+  });
+  $("#escudo").click(function(){
+    $(this).animate({height: "250px"});
+  });
+});
+
+$(document).ready(function(){
+  $("#escudo").hover(function(){
+    $(this).fadeTo(1000, 0.4);
+  });
+});
+</script>
     <style>
+        p{
+            text-align: center;
+        }
+       #escudo{
+           margin-left: 364px;
+       }
+        
+        button{
+            text-align: center;
+        }
         .btn-circle.btn-xl {
 
             margin: 50px;
@@ -20,8 +49,8 @@
             width: 70px;
             height: 70px;
             text-align: center;
-            border-radius: 35px;
-            font-size: 22px;
+            border-radius: 15px;
+            font-size: 18px;
             line-height: 1.33;
             position: center;
         }
@@ -39,40 +68,48 @@
             <h2 class="Encabezado"></h2>
 
             <header class="p-3 bg-success text-white">
-                <div class="container">
-                    <div class="d-flex flex-wrap align-items-center justify-content-center ">
+                <div class="container-fluid">
+                    <div class="d-flex flex-wrap  justify-content-center ">
                         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
                             <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
                                 <use xlink:href="#bootstrap"></use>
                             </svg>
                         </a>
+                        <div class="container-fluid">
                         <img src="../images/plaza_espanya.jpg" class="d-inline-block w-100" alt="12px" height="220px" >
-
-                        <div>
-                            <div class="row">
-                                <button type="button" text-allign="center" class="btn btn-primary btn-circle btn-xl"><i class="fa fa-list">
+                        </div>
+                        
+                            <div class="row text-white">
+                                <button type="button" text-align="center" class="btn btn-primary btn-circle btn-xl" onclick="location.href=''"><i class="fa fa-list">
                                         <p>juega</p>
                                     </i>
                                 </button>
-                                <button type="button" class="btn btn-dark btn-circle btn-xl"><i class="fa fa-link"></i>
+                                <button type="button" text-align="center" class="btn btn-secondary btn-circle btn-xl" onclick="location.href='scoreView.html'"><i class="fa fa-list">
+                                        <p>puntos</p>
+                                    </i>
                                 </button>
-                                <button type="button" class="btn btn-info btn-circle btn-xl"><i class="fa fa-check"></i>
+                                
                                 </button>
-                                <button type="button" class="btn btn-warning btn-circle btn-xl"><i class="fa fa-times"></i>
+                                <button type="button" class="btn btn-warning text-white btn-circle btn-xl"><i class="fa fa-heart"><p>rank</p></i>
                                 </button>
-                                <button type="button" class="btn btn-danger btn-circle btn-xl"><i class="fa fa-heart"></i>
+                                <button type="button" class="btn btn-danger btn-circle btn-xl" onclick="location.href='loginView.php'"><i class="fa fa-heart"><p>salir</p></i>
                                 </button>
                             </div>
-                        </div>
+
+                        
+                        
+                          
+                        
                         </form>
 
 
                     </div>
             </header>
 
-
-
-
+                <div class="container-fluid bg-white">
+                    <p><strong>Clicka o pasa el ratón por la imagen</strong></p>
+                        <img src="../images/escudo.png" id="escudo" class="d-inline-block w-50" text-align="center" alt="12px" height="220px" >
+                        </div>
 
         </div>
         <!-- /.panel-body -->
