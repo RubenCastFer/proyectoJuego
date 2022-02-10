317d87038af6e3212e38c68bf04999993f956f91
jesus-2808 <jesus.gonzalez.munoz.al@iespoligonosur.org>
2022-02-10 12:34:48 +0100

    menu juego

---
+++ views/menu.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"> </script>
+    <style>
+        .btn-circle.btn-xl {
+
+            margin: 50px;
+            align-items: center;
+            justify-content: center;
+
+            width: 70px;
+            height: 70px;
+            text-align: center;
+            border-radius: 35px;
+            font-size: 22px;
+            line-height: 1.33;
+            position: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+
+        </div>
+        <!-- /.panel-heading -->
+        <div class="panel-body">
+
+            <h2 class="Encabezado"></h2>
+
+            <header class="p-3 bg-success text-white">
+                <div class="container">
+                    <div class="d-flex flex-wrap align-items-center justify-content-center ">
+                        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap">
+                                <use xlink:href="#bootstrap"></use>
+                            </svg>
+                        </a>
+                        <img src="../images/plaza_espanya.jpg" class="d-inline-block w-100" alt="12px" height="220px" >
+
+                        <div>
+                            <div class="row">
+                                <button type="button" text-allign="center" class="btn btn-primary btn-circle btn-xl"><i class="fa fa-list">
+                                        <p>juega</p>
+                                    </i>
+                                </button>
+                                <button type="button" class="btn btn-dark btn-circle btn-xl"><i class="fa fa-link"></i>
+                                </button>
+                                <button type="button" class="btn btn-info btn-circle btn-xl"><i class="fa fa-check"></i>
+                                </button>
+                                <button type="button" class="btn btn-warning btn-circle btn-xl"><i class="fa fa-times"></i>
+                                </button>
+                                <button type="button" class="btn btn-danger btn-circle btn-xl"><i class="fa fa-heart"></i>
+                                </button>
+                            </div>
+                        </div>
+                        </form>
+
+
+                    </div>
+            </header>
+
+
+
+
+
+        </div>
+        <!-- /.panel-body -->
+    </div>
+</body>
+
+</html>
