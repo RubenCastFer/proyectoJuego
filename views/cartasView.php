f1409999be058c802cda7b091b3afc349b8eb915
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-02-14 13:40:13 +0100

    cartas creando estructura

--- views/cartasView.php
+++ views/cartasView.php
@@ -1,1 +1,23 @@
-<h1>cartas</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+     <!-- Latest compiled and minified CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"> </script>
+    <script src="../js/cartas.js" type="text/javascript"></script>
+    <title>Document</title>
+</head>
+<body>
+    <header class="container">
+    <div class="col-12"><h1>Encuentra las parejas</h1></div>
+        <div class=""></div>
+    </header>
+    <div class="container">         
+    </div>
+</body>
+</html>
