a1f327dc8032d2a15c29519c68629379da01c9c6
jesus-2808 <jesus.gonzalez.munoz.al@iespoligonosur.org>
2022-02-14 18:56:27 +0100

    relleno

--- models/juegoModel.php
+++ models/juegoModel.php
@@ -61,8 +61,8 @@ function getPuntuacionUser($usuario)
 $nivel=1;
 //rellenar array con fotos
 $nivel1=[1,2,3,4,5,6,7,8];
-$nivel2=[];
-$nivel3=[];
+$nivel2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+$nivel3=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32];
 
 switch ($nivel) {
     case '1':
