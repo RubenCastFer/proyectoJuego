2db8892f1b6d2350b6b71ce2de989e8c1ba83ba9
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-02-18 13:35:42 +0100

    guardar

--- js/cartas.js
+++ js/cartas.js
@@ -1,21 +1,21 @@
-            var ejec=0;
-            var click = 0;
-            var foto1;
-            var foto2;
-            var puntuacion=0;
-            var intentos=8;
-            const limite= 0
+var ejec = 0;
+var click = 0;
+var foto1;
+var foto2;
+var vida = sessionStorage.getItem("vida");
+var nivel = sessionStorage.getItem("nivel");
+var puntuacion = 0;
+var intentos = 8;
+const limite = 0
 $(function () {
 
-    // var valorNivel=$("#nivel").val();
-
-    // var parametros = {
-    //     nivel: valorNivel,
+    var parametros = {
+         nivel: nivel,
 
 
-    // };
+    };
     $.ajax({
-        // data: parametros,
+        data: parametros,
         url: './models/juegoModel.php',
         type: 'post',
         success: function (result) {
@@ -27,12 +27,11 @@ $(function () {
             cuerpo.append(div1);
             var contador = 0;
             for (let i = 0; i < json.length; i++) {
-                //for (let z = 0; z < 4; z++) {
                 var div2 = $("<div>", { "class": " col-2 mb-4 mt-4 card-grid" });
 
                 div1.append(div2);
                 var divImg1 = $("<div>", { "class": "frente tam oculto" })
-                var img1 = $("<img>", { "src": json[contador].img, "class": "w-100 h-100 " + json[contador].id, "alt":json[contador].name })
+                var img1 = $("<img>", { "src": json[contador].img, "class": "w-100 h-100 " + json[contador].id, "alt": json[contador].name })
                 divImg1.append(img1);
                 var divImg2 = $("<div>", { "class": "atras tam visible" })
                 var img2 = $("<img>", { "src": "./img/andalucia.png", "class": "w-100 h-100" })
@@ -44,15 +43,14 @@ $(function () {
 
                 contador++
 
-                //}
 
             }
-            
+
             //al hacer click sobre la imagen
             $("img").click(function (event) {
-                if (ejec==0) {
-                    ejec=1;  
-                    
+                if (ejec == 0) {
+                    ejec = 1;
+
                     if ($(this).parent().attr("class") == "atras tam visible") {
                         click++;
                         $(this).parent().addClass("rotar");
@@ -65,33 +63,34 @@ $(function () {
                             if (click == 1) {
                                 foto1 = $(this).parent().next().children();
                                 //console.log(click);
-                            } else if(click == 2){
+                            } else if (click == 2) {
                                 //console.log(click);
                                 click = 0;
                                 //console.log(click);
                                 foto2 = $(this).parent().next().children();
-                               
+
                                 //Comparo que las dos fotos vueltas sean iguales
                                 if (foto1.attr("class") == foto2.attr("class")) {
                                     // foto1 = "";
                                     // foto2 = "";
-                                    var liPunt=document.getElementById("puntuacion");
-                                    
-                                  
-                                   puntuacion+=100;
-                                   
-                                   liPunt.innerHTML="puntuacion " +puntuacion;
-                                   console.log(puntuacion);
-                                   swal(foto1.attr("alt"));
-                                  
+                                    var liPunt = document.getElementById("puntuacion");
+
+
+                                    puntuacion += 100;
+
+                                    liPunt.innerHTML = "puntuacion " + puntuacion;
+                                    console.log(puntuacion);
+                                    swal(foto1.attr("alt"));
+                                    sessionStorage.setItem("vida", vida);
+                                    sessionStorage.setItem("nivel", nivel++);
                                     //añadir puntuacion y un span con el nombre;
-                                   
-                                   
+
+
                                 } else {
                                     intentos--;
-                                    var liInt=document.getElementById("intentos");
-                                    liInt.innerHTML="intentos " +intentos;
-                                  
+                                    var liInt = document.getElementById("intentos");
+                                    liInt.innerHTML = "intentos " + intentos;
+
                                     foto1.parent().addClass("rotar");
                                     foto2.parent().addClass("rotar");
                                     setTimeout(() => {
@@ -100,16 +99,19 @@ $(function () {
                                         foto1.parent().removeClass("visible");
                                         foto1.parent().prev().addClass("visible");
                                         foto1.parent().addClass("oculto");
-    
+
                                         foto2.parent().removeClass("rotar");
                                         foto2.parent().prev().removeClass("oculto");
                                         foto2.parent().removeClass("visible");
                                         foto2.parent().prev().addClass("visible");
                                         foto2.parent().addClass("oculto");
-                                       if(intentos<=limite){ 
-                                           
-                                        swal("eres un pringao", "ni esta aciertas", "vaya malito");
-                                       }
+                                        if (intentos <= limite) {
+
+                                            swal("eres un pringao", "ni esta aciertas", "vaya malito");
+                                            sessionStorage.setItem("vida", --vida);
+                                            sessionStorage.setItem("nivel", "1");
+
+                                        }
 
                                         //quitar intento y si los intentos llegan a 0 restar una vida
                                     }, 1000);
@@ -117,15 +119,15 @@ $(function () {
                             }
                             // console.log("foto1 = " + foto1.attr("class"));
                             // console.log("foto2 = " + foto2.attr("class"));
-                            ejec=0; 
-                        }, 1400); 
+                            ejec = 0;
+                        }, 1400);
                     }
-                     
+
                 }
                 //console.log($(this).parent().attr("class"));
                 //console.log($(this).parent().next().children().attr("class"));
 
-                
+
             })
         }
     });
