ec35a97947622e079fdf061a9c8c0dd7ab0a79e3
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-02-18 10:33:05 +0100

    arreglado el giro por fin

--- js/cartas.js
+++ js/cartas.js
@@ -1,3 +1,7 @@
+            var ejec=0;
+            var click = 0;
+            var foto1;
+            var foto2;
 $(function () {
 
     // var valorNivel=$("#nivel").val();
@@ -40,96 +44,69 @@ $(function () {
                 //}
 
             }
-            var click = 0;
-            var foto1;
-            var foto2;
+            
             //al hacer click sobre la imagen
-            $("img").click(function () {
-                console.log($(this).parent().attr("class"));
+            $("img").click(function (event) {
+                if (ejec==0) {
+                    ejec=1;  
+                    
+                    if ($(this).parent().attr("class") == "atras tam visible") {
+                        click++;
+                        $(this).parent().addClass("rotar");
+                        setTimeout(() => {
+                            $(this).parent().removeClass("rotar");
+                            $(this).parent().addClass("oculto");
+                            $(this).parent().next().removeClass("oculto");
+                            $(this).parent().removeClass("visible");
+                            $(this).parent().next().addClass("visible")
+                            if (click == 1) {
+                                foto1 = $(this).parent().next().children();
+                                //console.log(click);
+                            } else if(click == 2){
+                                //console.log(click);
+                                click = 0;
+                                //console.log(click);
+                                foto2 = $(this).parent().next().children();
+                                
+                                //Comparo que las dos fotos vueltas sean iguales
+                                if (foto1.attr("class") == foto2.attr("class")) {
+                                    // foto1 = "";
+                                    // foto2 = "";
+
+                                    //añadir puntuacion y un span con el nombre;
+
+                                } else {
+                                    foto1.parent().addClass("rotar");
+                                    foto2.parent().addClass("rotar");
+                                    setTimeout(() => {
+                                        foto1.parent().removeClass("rotar");
+                                        foto1.parent().prev().removeClass("oculto");
+                                        foto1.parent().removeClass("visible");
+                                        foto1.parent().prev().addClass("visible");
+                                        foto1.parent().addClass("oculto");
+    
+                                        foto2.parent().removeClass("rotar");
+                                        foto2.parent().prev().removeClass("oculto");
+                                        foto2.parent().removeClass("visible");
+                                        foto2.parent().prev().addClass("visible");
+                                        foto2.parent().addClass("oculto");
+
+                                        //quitar intento y si los intentos llegan a 0 restar una vida
+                                    }, 1000);
+                                }
+                            }
+                            // console.log("foto1 = " + foto1.attr("class"));
+                            // console.log("foto2 = " + foto2.attr("class"));
+                            ejec=0; 
+                        }, 1400); 
+                    }
+                     
+                }
+                //console.log($(this).parent().attr("class"));
                 //console.log($(this).parent().next().children().attr("class"));
 
-                if ($(this).parent().attr("class") == "atras tam visible") {
-                    click++;
-                    $(this).parent().addClass("rotar");
-                    setTimeout(() => {
-                        $(this).parent().removeClass("rotar");
-                        $(this).parent().addClass("oculto");
-                        $(this).parent().next().removeClass("oculto");
-                        $(this).parent().removeClass("visible");
-                        $(this).parent().next().addClass("visible")
-                        if (click == 1) {
-                            foto1 = $(this).parent().next().children();
-
-                        } else if (click == 2) {
-                            click = 0;
-                            foto2 = $(this).parent().next().children();
-                            if (foto1.attr("class") == foto2.attr("class")) {
-                                // foto1 = "";
-                                // foto2 = "";
-                            } else {
-                                // foto1 = "";
-                                // foto2 = "";
-
-                                foto1.parent().addClass("rotar");
-                                foto2.parent().addClass("rotar");
-
-                                setTimeout(() => {
-                                    foto1.parent().removeClass("rotar");
-                                    foto1.parent().prev().removeClass("oculto");
-                                    foto1.parent().removeClass("visible");
-                                    foto1.parent().prev().addClass("visible");
-                                    foto1.parent().addClass("oculto");
-
-                                    foto2.parent().removeClass("rotar");
-                                    foto2.parent().prev().removeClass("oculto");
-                                    foto2.parent().removeClass("visible");
-                                    foto2.parent().prev().addClass("visible");
-                                    foto2.parent().addClass("oculto");
-                                }, 1000);
-
-
-                            }
-                        }
-                        console.log("foto1 = " + foto1.attr("class"));
-                        console.log("foto2 = " + foto2.attr("class"));
-                    }, 1400);
-                }
-
-
-
-
-
-                // $(this).parent().next().removeClass("oculto");
-                // $(this).parent().removeClass("visible");
-                // $(this).parent().next().addClass("visible")
-                // $(this).parent().addClass("oculto");
-                // $(this).parent().prev().addClass("visible");
-                // $(this).parent().addClass("oculto");
-                // $(this).parent().prev().removeClass("oculto");
-                // $(this).parent().removeClass("visible");
-
-
+                
             })
-
-
-
-
-
-
         }
-
     });
-
-
-
 });
-
-//.hasClass
-
-// function compararCartas(a, b) {
-//     if (a == b) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
