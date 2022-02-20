418a27b87fa26067c4d4bab8eefc4fb3beed1396
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2022-02-20 20:37:50 +0100

    puntuacion final

--- views/menuView.php
+++ views/menuView.php
@@ -160,6 +160,7 @@
                 <script>
                     sessionStorage.setItem("vida", "3");
                     sessionStorage.setItem("nivel", "1");
+                    sessionStorage.setItem("puntuacionFinal", "0");
                 </script>
 
 
